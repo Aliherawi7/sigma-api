2d011b48e29be708f05e96c9f1ea13a749bcccd0
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-22 01:46:31 +0430

    necessary fileds added

--- src/main/java/com/herawi/heraynet/model/ProfileImage.java
+++ src/main/java/com/herawi/heraynet/model/ProfileImage.java
@@ -1,4 +1,22 @@
 package com.herawi.heraynet.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class ProfileImage {
+    @Id
+    private long userId;
+    @Lob
+    private byte[] image;
 }
