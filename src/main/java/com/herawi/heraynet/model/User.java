61b3f4c2ec036a2dcae1c4f05956dae72925375e
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-22 01:27:24 +0430

    constructors, getters and setters added

--- src/main/java/com/herawi/heraynet/model/User.java
+++ src/main/java/com/herawi/heraynet/model/User.java
@@ -1,11 +1,6 @@
 package com.herawi.heraynet.model;
 
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.time.LocalDate;
@@ -30,4 +25,142 @@ public class User {
     private Date lockTime;
     private String location;
 
+    public User(long id) {
+        this.id = id;
+    }
+    public User() {
+    }
+
+    public User(long id, String name, String lastName, LocalDate dob, boolean isActive, String email, String password, String phoneNumber, String userName, boolean accountLocked, int failedAttempt, Date lockTime, String location) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.joinedDate = LocalDateTime.now();
+        this.isActive = isActive;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.userName = userName;
+        this.accountLocked = accountLocked;
+        this.failedAttempt = failedAttempt;
+        this.lockTime = lockTime;
+        this.location = location;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name.toLowerCase().trim();
+    }
+
+    public String getLastName() {
+        return lastName.toLowerCase().trim();
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName.toLowerCase().trim();
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public LocalDateTime getJoinedDate() {
+        return joinedDate;
+    }
+
+    public void setJoinedDate(LocalDateTime joinedDate) {
+        this.joinedDate = joinedDate;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email.toLowerCase().trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName.trim().toLowerCase();
+    }
+
+    public boolean isAccountLocked() {
+        return accountLocked;
+    }
+
+    public void setAccountLocked(boolean accountLocked) {
+        this.accountLocked = accountLocked;
+    }
+
+    public int getFailedAttempt() {
+        return failedAttempt;
+    }
+
+    public void setFailedAttempt(int failedAttempt) {
+        this.failedAttempt = failedAttempt;
+    }
+
+    public Date getLockTime() {
+        return lockTime;
+    }
+
+    public void setLockTime(Date lockTime) {
+        this.lockTime = lockTime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location.trim().toLowerCase();
+    }
+
+    public int getAge(){
+        return LocalDate.now().getYear() - dob.getYear();
+    }
 }
