878f7ac1332d2caa34074299b32d7536781f0bd6
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-22 01:05:09 +0430

    project initialized

---
+++ src/test/java/com/herawi/heraynet/HeraynetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herawi.heraynet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeraynetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
