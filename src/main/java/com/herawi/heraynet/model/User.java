dcdb9e4b79ac6c9f11f792087182c51cb490cda1
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-22 01:18:24 +0430

    most important fields added in user class

--- src/main/java/com/herawi/heraynet/model/User.java
+++ src/main/java/com/herawi/heraynet/model/User.java
@@ -1,4 +1,33 @@
 package com.herawi.heraynet.model;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
 public class User {
+    @Id
+    private long id;
+    private String name;
+    private String lastName;
+    private LocalDate dob;
+    private LocalDateTime joinedDate;
+    private boolean isActive;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String userName;
+    private boolean accountLocked;
+    private int failedAttempt;
+    private Date lockTime;
+    private String location;
+
 }
