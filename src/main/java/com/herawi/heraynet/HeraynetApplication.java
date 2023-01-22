a6510e23e5d9c95a06640d5a3285e682ab5eff78
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-22 22:01:59 +0430

    BcryptPassword encoder object bean created

--- src/main/java/com/herawi/heraynet/HeraynetApplication.java
+++ src/main/java/com/herawi/heraynet/HeraynetApplication.java
@@ -2,13 +2,23 @@ package com.herawi.heraynet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 
 public class HeraynetApplication {
 
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoderBean() {
+		return new BCryptPasswordEncoder();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(HeraynetApplication.class, args);
 	}
+
+
 
 }
