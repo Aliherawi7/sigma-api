4aee3f6daaa9bf1c967549d56b4dafac10b4168d
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-23 22:57:42 +0430

    one static account added in cammondlinerunner method

--- src/main/java/com/herawi/sigma/SigmaApplication.java
+++ src/main/java/com/herawi/sigma/SigmaApplication.java
@@ -1,9 +1,18 @@
 package com.herawi.sigma;
 
+import com.herawi.sigma.model.Account;
+import com.herawi.sigma.model.ProfileImage;
+import com.herawi.sigma.service.AccountService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.time.LocalDate;
 
 @SpringBootApplication
 
@@ -17,6 +26,23 @@ public class SigmaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SigmaApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(AccountService accountService){
+
+		return args -> {
+			Account account = new Account(
+					1L, "Ali", "Herawi", LocalDate.of(2000,3,12),
+					true, "aliherawi7@gmail.com","12345","0797608705",
+					"aliherawi7",false, 0, null, "Afg",
+					'M'
+			);
+			File file1 = new File("src/main/resources/templates/images/ProfileImages/user1.jpg");
+			FileInputStream fis = new FileInputStream(file1);
+			byte[] file1Bytes = new byte[(int) file1.length()];
+			accountService.addAccount(account, file1Bytes);
+		};
 	}
 
 
