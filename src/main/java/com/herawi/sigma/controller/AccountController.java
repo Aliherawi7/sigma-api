8517bc290b3c3e146dbecd8f1f12c64de7d8ccb8
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-23 23:01:26 +0430

    simple method added for test

--- src/main/java/com/herawi/sigma/controller/AccountController.java
+++ src/main/java/com/herawi/sigma/controller/AccountController.java
@@ -1,13 +1,16 @@
 package com.herawi.sigma.controller;
 
+import com.herawi.sigma.dto.AccountInfo;
 import com.herawi.sigma.service.AccountService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
-@RequestMapping("api/person")
+@RequestMapping("api/account")
 public class AccountController {
 
     private final AccountService accountService;
@@ -16,8 +19,21 @@ public class AccountController {
         this.accountService = accountService;
     }
 
+    @GetMapping
     public ResponseEntity<?> getUser(HttpServletRequest request){
-
+        AccountInfo accountInfo = accountService.getAccount(request);
+        if(accountInfo != null){
+            return ResponseEntity.ok().body(accountInfo);
+        }
         return ResponseEntity.noContent().build();
     }
+    @GetMapping("/{email}")
+    public ResponseEntity<?> getUsers(@PathVariable String email){
+        AccountInfo accountInfo = accountService.getAccount(email);
+        if(accountInfo != null){
+            return ResponseEntity.ok().body(accountInfo);
+        }
+        return ResponseEntity.noContent().build();
+    }
+
 }
