65b35bfc07527ae4e9e8221e9c9613fca1744180
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-23 13:54:40 +0430

    new quesry methods added

--- src/main/java/com/herawi/sigma/repository/AccountRepository.java
+++ src/main/java/com/herawi/sigma/repository/AccountRepository.java
@@ -1,12 +1,12 @@
 package com.herawi.sigma.repository;
 
 import com.herawi.sigma.model.Account;
-import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Collection;
 
-public interface AccountRepository extends CrudRepository<Account, Long> {
-    Account getPersonByEmail(String email);
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Account findByEmail(String email);
+    Account findByUserName(String userName);
     Collection<Account> findAllByNameOrLastName(String name, String lastName);
     boolean existsByEmailOrPhoneNumber(String email, String phoneNumber);
     boolean existsByEmailOrUserName(String email, String userName);
