0fc889da7b9b0b73de03a45a816e0760286c5608
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-08 20:34:20 -0800

    this class is used to map the Account object into AccountDTO object

--- src/main/java/com/herawi/sigma/service/AccountDTOMapper.java
+++ src/main/java/com/herawi/sigma/service/AccountDTOMapper.java
@@ -1,4 +1,19 @@
 package com.herawi.sigma.service;
 
-public class AcountDtoMapper {
+import com.herawi.sigma.dto.AccountDTO;
+import com.herawi.sigma.model.Account;
+import com.herawi.sigma.model.ProfileImage;
+
+import java.util.function.Function;
+
+public class AccountDTOMapper {
+    public static AccountDTO apply(Account account, ProfileImage profileImage) {
+        return  new AccountDTO(
+                account.getName(),
+                account.getLastName(),
+                profileImage.getImage(),
+                account.getEmail(),
+                account.getConnections().size()
+        );
+    }
 }
