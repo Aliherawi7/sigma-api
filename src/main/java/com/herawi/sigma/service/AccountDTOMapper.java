8248e224868b6769490b8335a1c3e8e9cf7e1489
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-10 00:02:01 -0800

    MultipartFile object type changed to byte array

--- src/main/java/com/herawi/sigma/service/AccountDTOMapper.java
+++ src/main/java/com/herawi/sigma/service/AccountDTOMapper.java
@@ -2,16 +2,14 @@ package com.herawi.sigma.service;
 
 import com.herawi.sigma.dto.AccountDTO;
 import com.herawi.sigma.model.Account;
-import com.herawi.sigma.model.ProfileImage;
 
-import java.util.function.Function;
 
 public class AccountDTOMapper {
-    public static AccountDTO apply(Account account, ProfileImage profileImage) {
+    public static AccountDTO apply(Account account, byte[] profileImage) {
         return  new AccountDTO(
                 account.getName(),
                 account.getLastName(),
-                profileImage.getImage(),
+                profileImage,
                 account.getEmail(),
                 account.getConnections().size()
         );
