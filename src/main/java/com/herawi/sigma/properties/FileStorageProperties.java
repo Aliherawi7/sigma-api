401aad46b7e06e95b04419e296edc8657d1e8f7c
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-09 23:56:04 -0800

    this the global dir address for files

--- src/main/java/com/herawi/sigma/properties/FileStorageProperties.java
+++ src/main/java/com/herawi/sigma/properties/FileStorageProperties.java
@@ -1,4 +1,23 @@
 package com.herawi.sigma.properties;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class FileStorageProperties {
+
+
+    private String uploadDir;
+
+    public FileStorageProperties(@Value("${file.uploadDir}") String uploadDir) {
+        this.uploadDir = uploadDir;
+    }
+
+    public String getUploadDir() {
+        return uploadDir;
+    }
+
+    public void setUploadDir(String uploadDir) {
+        this.uploadDir = uploadDir;
+    }
 }
