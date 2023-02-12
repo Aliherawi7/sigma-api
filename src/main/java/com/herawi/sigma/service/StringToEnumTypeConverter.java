e81e5e6b19b3788428d20e6bcd39e99251d47121
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-11 19:37:35 -0800

    this class is used to convert string to gender enum type

--- src/main/java/com/herawi/sigma/service/StringToEnumTypeConverter.java
+++ src/main/java/com/herawi/sigma/service/StringToEnumTypeConverter.java
@@ -1,4 +1,12 @@
 package com.herawi.sigma.service;
+import com.herawi.sigma.constants.Gender;
+import org.springframework.core.convert.converter.Converter;
 
-public class StringToEnumTypeConverter {
+public class StringToEnumTypeConverter implements Converter<String, Gender> {
+
+    @Override
+    public Gender convert(String source) {
+        return Gender.decode(source.toUpperCase());
+    }
+
 }
