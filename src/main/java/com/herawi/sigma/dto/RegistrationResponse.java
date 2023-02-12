c48b1e95b71f441832e02dade1551fd79624ed85
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-11 21:49:29 -0800

    this class is going to be used while the user register successfully

--- src/main/java/com/herawi/sigma/dto/RegistrationResponse.java
+++ src/main/java/com/herawi/sigma/dto/RegistrationResponse.java
@@ -1,4 +1,27 @@
 package com.herawi.sigma.dto;
 
 public class RegistrationResponse {
+    private String accessToken;
+    private AccountDTO accountDTO;
+
+    public RegistrationResponse(String accessToken, AccountDTO accountDTO) {
+        this.accessToken = accessToken;
+        this.accountDTO = accountDTO;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public AccountDTO getAccountDTO() {
+        return accountDTO;
+    }
+
+    public void setAccountDTO(AccountDTO accountDTO) {
+        this.accountDTO = accountDTO;
+    }
 }
