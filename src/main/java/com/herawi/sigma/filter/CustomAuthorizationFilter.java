8709b75ecb38827fa0a9f833ace2be528b2c86ff
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-12 00:07:52 -0800

    simple impl added

--- src/main/java/com/herawi/sigma/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/herawi/sigma/filter/CustomAuthorizationFilter.java
@@ -4,16 +4,18 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class CustomeAuthorizationFilter extends OncePerRequestFilter {
+public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (request.getServletPath().equals("/api/login")) {
-            filterChain. doFilter(request, response);
-        }
+        filterChain.doFilter(request, response);
+//        if (request.getServletPath().equals("/api/login")) {
+//            filterChain.doFilter(request, response);
+//        }
     }
 }
