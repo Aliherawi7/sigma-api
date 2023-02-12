f7c2ea960a5ca8986cb472c01150283663acec0b
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-11 19:39:47 -0800

    role entity created to controlling account permissions

--- src/main/java/com/herawi/sigma/model/Role.java
+++ src/main/java/com/herawi/sigma/model/Role.java
@@ -1,4 +1,37 @@
 package com.herawi.sigma.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Role {
+    @Id
+    private int id;
+    @Column(nullable = false)
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
