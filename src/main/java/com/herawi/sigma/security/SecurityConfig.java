980764126e410ec063a4f3beecb69b5c1c26cc7b
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-12 00:10:38 -0800

    CustomAuthenticationFilter and CustomeAthorizationFilter congurated and added to security config

--- src/main/java/com/herawi/sigma/security/SecurityConfig.java
+++ src/main/java/com/herawi/sigma/security/SecurityConfig.java
@@ -1,23 +1,32 @@
 package com.herawi.sigma.security;
 
+import com.herawi.sigma.filter.CustomAuthenticationFilter;
+import com.herawi.sigma.filter.CustomAuthorizationFilter;
+import com.herawi.sigma.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    // this bean is for doing authentication in authentication filter
     private final UserDetailsService userDetailsService;
+    private final AccountService accountService;
 
     @Autowired
-    public SecurityConfig(BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailsService userDetailsService) {
+    public SecurityConfig(BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailsService userDetailsService,AccountService accountService ) {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.userDetailsService = userDetailsService;
+        this.accountService = accountService;
     }
 
     @Override
@@ -27,8 +36,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter =
+                new CustomAuthenticationFilter(accountService, authenticationManager());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
         http.csrf().disable();
         http.cors().disable();
-        http.authorizeHttpRequests().antMatchers("/*").permitAll();
+        http.authorizeHttpRequests().antMatchers("api/*","**").permitAll();
+        http.authorizeHttpRequests().antMatchers("/api/login","/api/register").permitAll();
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
