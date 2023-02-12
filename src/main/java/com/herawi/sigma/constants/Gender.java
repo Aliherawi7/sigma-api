77d30f039d397e63e12f2ad30eb92e4fd3644679
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-11 19:35:52 -0800

    Gender constant created for controlling the account gender type

--- src/main/java/com/herawi/sigma/constants/Gender.java
+++ src/main/java/com/herawi/sigma/constants/Gender.java
@@ -1,4 +1,25 @@
 package com.herawi.sigma.constants;
 
-public class Gender {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.stream.Stream;
+
+public enum  Gender {
+    MALE("MALE"),
+    FEMALE("FEMALE");
+
+    final String value;
+    Gender(String value){
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+    @JsonCreator
+    public static Gender decode(final String value){
+        return Stream.of(Gender.values()).filter(targetEnum -> targetEnum.value.equals(value.toUpperCase())).findFirst().orElse(null);
+    }
 }
