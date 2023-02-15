009d4ad99315d14d85c854d540fddb00e69d0f44
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-15 01:55:52 -0800

    cors configed

--- src/main/java/com/herawi/sigma/security/SecurityConfig.java
+++ src/main/java/com/herawi/sigma/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -40,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 new CustomAuthenticationFilter(accountService, authenticationManager());
         customAuthenticationFilter.setFilterProcessesUrl("/api/login");
         http.csrf().disable();
-        http.cors().disable();
+        http.cors();
         http.authorizeHttpRequests().antMatchers("api/*","**").permitAll();
         http.authorizeHttpRequests().antMatchers("/api/login","/api/register").permitAll();
         http.addFilter(customAuthenticationFilter);
