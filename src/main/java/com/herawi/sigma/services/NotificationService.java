057a4b2054d3bc15db9050f6d0c7dd247d0df471
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-23 15:24:34 -0800

    getAllNotifications and getAllSeenNotifications methods implemented

--- src/main/java/com/herawi/sigma/services/NotificationService.java
+++ src/main/java/com/herawi/sigma/services/NotificationService.java
@@ -1,4 +1,51 @@
 package com.herawi.sigma.services;
 
+import com.herawi.sigma.models.Account;
+import com.herawi.sigma.models.Notification;
+import com.herawi.sigma.repositories.NotificationRepository;
+import com.herawi.sigma.utils.JWTTools;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Service
 public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+    private final AccountService accountService;
+    @Autowired
+    public NotificationService(NotificationRepository notificationRepository, AccountService accountService) {
+        this.notificationRepository = notificationRepository;
+        this.accountService = accountService;
+    }
+
+    public Collection<Notification> getAllNotifications(HttpServletRequest request){
+        String email = JWTTools.getUserEmailByJWT(request);
+        Collection<Notification> notifications = new ArrayList<>();
+        Account account = null;
+        if(email != null){
+            account = accountService.getAccountWithDetails(email);
+            System.out.println("user id: "+account.getId());
+        }
+        if(account != null){
+            notifications = notificationRepository.findAllByUserId(account.getId());
+        }
+        return notifications;
+    }
+
+    public Collection<Notification> getAllUnSeenNotification(HttpServletRequest request){
+        String email = JWTTools.getUserEmailByJWT(request);
+        Collection<Notification> notifications = new ArrayList<>();
+        Account account = null;
+        if(email != null){
+            account = accountService.getAccountWithDetails(email);
+        }
+        if(account != null){
+            notifications = notificationRepository.findAllBySeenFalseAndUserId(account.getId());
+        }
+        return notifications;
+    }
 }
