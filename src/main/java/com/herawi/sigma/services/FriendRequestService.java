fd277d966ff925d6288d893ab46b813f4f2ca644
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-24 19:00:50 -0800

    getAllsent and getAllRecieve methods implemented

--- src/main/java/com/herawi/sigma/services/FriendRequestService.java
+++ src/main/java/com/herawi/sigma/services/FriendRequestService.java
@@ -5,21 +5,35 @@ import com.herawi.sigma.repositories.FriendRequestRepository;
 import org.springframework.stereotype.Service;
 
 import java.security.InvalidParameterException;
+import java.util.Collection;
 
 @Service
 public class FriendRequestService {
 
-    private final FriendRequestRepository connectionRequestRepository;
+    private final FriendRequestRepository friendRequestRepository;
 
-    public FriendRequestService(FriendRequestRepository connectionRequestRepository) {
-        this.connectionRequestRepository = connectionRequestRepository;
+    public FriendRequestService(FriendRequestRepository friendRequestRepository) {
+        this.friendRequestRepository = friendRequestRepository;
+    }
+
+    /*
+    * get all friend requests by senderId
+    * */
+    public Collection<FriendRequest> getAllFriendRequestsBySenderId(long senderId){
+        return friendRequestRepository.findAllByRequestSenderId(senderId);
+    }
+    /*
+    * get all friend requests by receiverId
+    * */
+    public Collection<FriendRequest> getAllFriendRequestsByReceiverId(long receiverId){
+        return friendRequestRepository.findAllByRequestReceiverId(receiverId);
     }
 
     public FriendRequest addFriendRequest(FriendRequest friendRequest){
         if(friendRequest.getRequestReceiverId() == friendRequest.getRequestSenderId())
             throw new InvalidParameterException("Invalid receiverId");
 
-        return connectionRequestRepository.save(friendRequest);
+        return friendRequestRepository.save(friendRequest);
     }
 
 }
