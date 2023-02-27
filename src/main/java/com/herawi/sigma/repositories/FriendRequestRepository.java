a66c3ce8bf83f5efcf479baa0aaee93b69a3d3ed
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-26 19:14:13 -0800

    useful query methods added

--- src/main/java/com/herawi/sigma/repositories/FriendRequestRepository.java
+++ src/main/java/com/herawi/sigma/repositories/FriendRequestRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Collection;
 
 public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
-    Collection<FriendRequest> findAllByRequestReceiverId(long receiverId);
-    Collection<FriendRequest> findAllByRequestSenderId(long senderId);
-
+    Collection<FriendRequest> findAllByRequestReceiverUserName(String receiverUserName);
+    Collection<FriendRequest> findAllByRequestSenderUserName(String senderUserName);
+    FriendRequest findByRequestReceiverUserNameAndRequestSenderUserName(String receiverUserName, String senderUserName);
+    void deleteByRequestSenderUserNameAndRequestReceiverUserName(String requestSender, String requestReceiver);
 }
