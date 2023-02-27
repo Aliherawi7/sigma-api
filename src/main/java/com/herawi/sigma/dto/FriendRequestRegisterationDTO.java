6262c83515a5da43ddf6b6d6deb02607acc341ad
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-26 19:12:49 -0800

    friendRequestRegisterationDTO created to get and control the friend request parameters

--- src/main/java/com/herawi/sigma/dto/FriendRequestRegisterationDTO.java
+++ src/main/java/com/herawi/sigma/dto/FriendRequestRegisterationDTO.java
@@ -1,4 +1,27 @@
 package com.herawi.sigma.dto;
 
 public class FriendRequestRegisterationDTO {
+    private String requestReceiverUserName;
+    private String requestSenderUserName;
+
+    public FriendRequestRegisterationDTO(String requestReceiverUserName, String requestSenderUserName) {
+        this.requestReceiverUserName = requestReceiverUserName;
+        this.requestSenderUserName = requestSenderUserName;
+    }
+
+    public String getRequestReceiverUserName() {
+        return requestReceiverUserName;
+    }
+
+    public void setRequestReceiverUserName(String requestReceiverUserName) {
+        this.requestReceiverUserName = requestReceiverUserName;
+    }
+
+    public String getRequestSenderUserName() {
+        return requestSenderUserName;
+    }
+
+    public void setRequestSenderUserName(String requestSenderUserName) {
+        this.requestSenderUserName = requestSenderUserName;
+    }
 }
