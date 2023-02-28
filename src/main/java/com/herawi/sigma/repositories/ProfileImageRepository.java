835530ce7d07bb015e4e6297a6ff477fedaaddda
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-28 11:51:39 -0800

    unused entity and repo removed

--- src/main/java/com/herawi/sigma/repositories/ProfileImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.herawi.sigma.repositories;
-
-import com.herawi.sigma.models.ProfileImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileImageRepository extends JpaRepository<ProfileImage, Long> {
-}
