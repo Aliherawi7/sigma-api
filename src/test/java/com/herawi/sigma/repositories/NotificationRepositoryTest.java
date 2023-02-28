8f91707fba534dae2d5bfdb1c029cc3cbe43cbdb
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-28 11:50:34 -0800

    all methods in NotificationRespository tested

--- src/test/java/com/herawi/sigma/repositories/NotificationRepositoryTest.java
+++ src/test/java/com/herawi/sigma/repositories/NotificationRepositoryTest.java
@@ -1,5 +1,59 @@
+package com.herawi.sigma.repositories;
+
+import com.herawi.sigma.models.Notification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class NotificationRepositoryTest {
 
+    @Autowired
+    private NotificationRepository underTest;
+    private Notification notification;
+
+    @BeforeEach
+    void setUp() {
+        notification = new Notification();
+        notification.setUserName("alex_parker");
+        notification.setMessage("You have a new friend request");
+        notification.setTitle("friend Request");
+    }
+
+    @Test
+    void findAllByUserName() {
+        //given
+        underTest.save(notification);
+
+        //when
+        String userName = notification.getUserName();
+
+        //then
+        assertTrue(underTest.findAllByUserName(userName)
+                .stream()
+                .allMatch(item -> item.getUserName().equalsIgnoreCase(userName))
+        );
+    }
+
+    @Test
+    void findAllBySeenFalseAndUserName() {
+        // given
+        notification.setSeen(false);
+        underTest.save(notification);
+
+        //when
+
+        String userName = notification.getUserName();
+        boolean isSeen = notification.isSeen();
+
+        //then
+        assertTrue(underTest.findAllBySeenFalseAndUserName(userName)
+                .stream()
+                .allMatch(item ->
+                        item.isSeen() == isSeen && item.getUserName().equalsIgnoreCase(userName))
+        );
+    }
 }
