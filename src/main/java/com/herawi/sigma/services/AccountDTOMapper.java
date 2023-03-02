6b3052b235e2fca3bf2090c019ec8c91dbb51c57
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-02 12:29:12 -0800

    connection updated to friend

--- src/main/java/com/herawi/sigma/services/AccountDTOMapper.java
+++ src/main/java/com/herawi/sigma/services/AccountDTOMapper.java
@@ -12,7 +12,7 @@ public class AccountDTOMapper {
                 account.getUserName(),
                 profileImage,
                 account.getEmail(),
-                account.getConnections().size(),
+                account.getFriends().size(),
                 account.getGender()
         );
     }
