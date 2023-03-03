b7f45e1f4b847c5209a45e644006f56ecad575f8
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-02 17:29:07 -0800

    custome exception class if there is not such account in DB"

--- src/main/java/com/herawi/sigma/exceptions/AccountNotFoundException.java
+++ src/main/java/com/herawi/sigma/exceptions/AccountNotFoundException.java
@@ -1,4 +1,7 @@
 package com.herawi.sigma.exceptions;
 
-public class AccountNotFoundException {
+public class AccountNotFoundException extends RuntimeException{
+    public AccountNotFoundException(String message) {
+        super(message);
+    }
 }
