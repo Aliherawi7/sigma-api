aca252bbf0ba1af6e99ca9486f3ad7d5250d6970
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-04 16:15:30 -0800

    simple end-points added

--- src/main/java/com/herawi/sigma/controllers/MessageController.java
+++ src/main/java/com/herawi/sigma/controllers/MessageController.java
@@ -1,4 +1,42 @@
 package com.herawi.sigma.controllers;
 
+import com.herawi.sigma.models.Message;
+import com.herawi.sigma.services.MessageService;
+import com.herawi.sigma.utils.JWTTools;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+
+@RestController
+@RequestMapping("api/v1/messages")
 public class MessageController {
+
+    private final MessageService messageService;
+
+    public MessageController(MessageService messageService){
+        this.messageService = messageService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllMessages(HttpServletRequest request){
+        String userName = JWTTools.getUsernameByJWT(request);
+        Collection<Message> messages = messageService.getAllByReceiverUsername(userName);
+        return ResponseEntity.ok(messages);
+    }
+
+    @GetMapping("/{userName}")
+    public ResponseEntity<?> getAllMessagesByUsername(HttpServletRequest request, @PathVariable("userName") String userName){
+        String currentUser = JWTTools.getUsernameByJWT(request);
+        Collection<Message> messages = messageService
+                .getAllMessageBySenderUsernameAndReceiverUsername(userName, currentUser);
+        return ResponseEntity.ok(
+        messages.stream().sorted());
+    }
+
+
 }
