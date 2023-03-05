1050f2905137b2c96366d01e125e4b289240fe14
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-04 16:13:53 -0800

    useful query methods added

--- src/main/java/com/herawi/sigma/repositories/MessageRepository.java
+++ src/main/java/com/herawi/sigma/repositories/MessageRepository.java
@@ -1,4 +1,12 @@
 package com.herawi.sigma.repositories;
 
-public interface MessageRepository {
+import com.herawi.sigma.models.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Collection;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    Collection<Message> findAllBySenderUsername(String senderUsername);
+    Collection<Message> findAllByReceiverUsername(String receiverUsername);
+    Collection<Message> findAllBySenderUsernameAndReceiverUsername(String senderUsername, String receiverUsername);
+
 }
