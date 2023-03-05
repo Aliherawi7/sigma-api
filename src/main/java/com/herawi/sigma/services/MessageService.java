74e5c5bdb0e42eaec6bb44f3f565b77fb3bcef6c
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-04 16:14:39 -0800

    service for message entity implemented

--- src/main/java/com/herawi/sigma/services/MessageService.java
+++ src/main/java/com/herawi/sigma/services/MessageService.java
@@ -1,4 +1,40 @@
 package com.herawi.sigma.services;
 
+
+import com.herawi.sigma.models.Message;
+import com.herawi.sigma.repositories.MessageRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
 public class MessageService {
+
+    private final MessageRepository messageRepository;
+
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+
+    public void addMessage(Message message){
+        messageRepository.save(message);
+    }
+
+    public Collection<Message> getAllMessageBySenderUsernameAndReceiverUsername(String senderUsername, String receiverUsername){
+        return messageRepository.findAllBySenderUsernameAndReceiverUsername(senderUsername, receiverUsername);
+    }
+
+    public Collection<Message> getAllBySenderUsername(String senderUsername){
+        return messageRepository.findAllBySenderUsername(senderUsername);
+    }
+
+    public Collection<Message> getAllByReceiverUsername(String receiverName){
+        return messageRepository.findAllByReceiverUsername(receiverName)
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
 }
