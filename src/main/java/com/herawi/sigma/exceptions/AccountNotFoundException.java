cf5f464ba527431cf497faacf3e2d4977185645c
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-06 22:46:23 -0800

    this runtime exception is going to be thrown while there is no account by provided email

--- src/main/java/com/herawi/sigma/exceptions/AccountNotFoundException.java
+++ src/main/java/com/herawi/sigma/exceptions/AccountNotFoundException.java
@@ -1,6 +1,11 @@
 package com.herawi.sigma.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class AccountNotFoundException extends RuntimeException{
+
     public AccountNotFoundException(String message) {
         super(message);
     }
