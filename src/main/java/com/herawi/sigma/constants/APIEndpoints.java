c716ee83f80c802c75c62ba6aede68c71d4016fb
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-06 22:43:53 -0800

    profile picture url path added

--- src/main/java/com/herawi/sigma/constants/APIEndpoints.java
+++ src/main/java/com/herawi/sigma/constants/APIEndpoints.java
@@ -1,4 +1,14 @@
 package com.herawi.sigma.constants;
 
 public enum APIEndpoints {
+    PROFILE_PICTURE("api/v1/files/profile-picture/");
+
+    final String value;
+    APIEndpoints(String value){
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
