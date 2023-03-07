daca31965e64d4e180bcfd06164b5f5c3150abd2
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-06 22:47:25 -0800

    profile-picture endpoint added to unuthorized paths

--- src/main/java/com/herawi/sigma/filters/CustomAuthorizationFilter.java
+++ src/main/java/com/herawi/sigma/filters/CustomAuthorizationFilter.java
@@ -30,7 +30,9 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        if (request.getServletPath().equals("/api/login") || request.getServletPath().equals("/api/accounts")) {
+        if (request.getServletPath().equals("/api/v1/login") ||
+            request.getServletPath().equals("/api/v1/accounts") ||
+            request.getServletPath().startsWith("/api/v1/files/profile-picture/")) {
             filterChain.doFilter(request, response);
         }else {
             String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
