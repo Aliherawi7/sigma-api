3795acddbe70e52f94a0d274cab11fb790d0ba49
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-12 16:53:56 +0430

    this exception is going to set the http status code to 404 and not dound message

--- src/main/java/com/herawi/sigma/exceptions/AccountNotFoundException.java
+++ src/main/java/com/herawi/sigma/exceptions/AccountNotFoundException.java
@@ -3,7 +3,7 @@ package com.herawi.sigma.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class AccountNotFoundException extends RuntimeException{
 
     public AccountNotFoundException(String message) {
