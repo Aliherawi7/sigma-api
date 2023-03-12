2c3eb269948fdff8c16cb66012fc3622b480f8ce
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-12 16:55:22 +0430

    this entity class is going to be as DAO for post entity

--- src/main/java/com/herawi/sigma/models/Post.java
+++ src/main/java/com/herawi/sigma/models/Post.java
@@ -1,4 +1,20 @@
 package com.herawi.sigma.models;
 
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.Collection;
+
 public class Post {
+
+    @Id
+    private Long id;
+    private String username;
+    private LocalDateTime localDateTime;
+    private Long reactions;
+    private String textContent;
+    @OneToMany(fetch = FetchType.EAGER)
+    private Collection<String> images;
+
 }
