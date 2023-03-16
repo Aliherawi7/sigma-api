ec26e3e81b573284aacd8fb49b769646252de41e
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-16 16:05:37 +0430

    endpoint added for saving and retriving messages

--- src/main/java/com/herawi/sigma/controllers/MessageController.java
+++ src/main/java/com/herawi/sigma/controllers/MessageController.java
@@ -1,13 +1,13 @@
 package com.herawi.sigma.controllers;
 
+import com.herawi.sigma.dto.MessageRequestDO;
 import com.herawi.sigma.models.Message;
 import com.herawi.sigma.services.MessageService;
 import com.herawi.sigma.utils.JWTTools;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Collection;
@@ -18,7 +18,8 @@ public class MessageController {
 
     private final MessageService messageService;
 
-    public MessageController(MessageService messageService){
+    @Autowired
+    public MessageController(MessageService messageService) {
         this.messageService = messageService;
     }
 
@@ -30,13 +31,25 @@ public class MessageController {
     }
 
     @GetMapping("/{userName}")
-    public ResponseEntity<?> getAllMessagesByUsername(HttpServletRequest request, @PathVariable("userName") String userName){
+    public ResponseEntity<?> getAllMessagesByUsername(HttpServletRequest request,
+                                                      @PathVariable("userName") String userName){
         String currentUser = JWTTools.getUsernameByJWT(request);
         Collection<Message> messages = messageService
                 .getAllMessageBySenderUsernameAndReceiverUsername(userName, currentUser);
         return ResponseEntity.ok(
         messages.stream().sorted());
     }
+
+    @PostMapping
+    public ResponseEntity<Message> sendMessageTo(HttpServletRequest request, @RequestBody MessageRequestDO messageRequestDO){
+        return new ResponseEntity<>(
+                messageService.addMessage(request, messageRequestDO),
+                HttpStatus.CREATED);
+    }
+
+
+
+
 
 
 }
