776d3cff4baef7a9138f4ddd2aa16a982b648b9b
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-16 16:04:19 +0430

    this Exception will be thrown while the email or password is wrong

--- src/main/java/com/herawi/sigma/exceptions/CredentialException.java
+++ src/main/java/com/herawi/sigma/exceptions/CredentialException.java
@@ -1,5 +1,10 @@
 package com.herawi.sigma.exceptions;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class CredentialException extends RuntimeException {
     public CredentialException(String message) {
         super(message);
