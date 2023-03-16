775726fc00dbd2efe531798fbafb049b8ca70466
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-16 16:02:08 +0430

    add method implemented

--- src/main/java/com/herawi/sigma/services/MessageService.java
+++ src/main/java/com/herawi/sigma/services/MessageService.java
@@ -1,10 +1,16 @@
 package com.herawi.sigma.services;
 
 
+import antlr.debug.MessageAdapter;
+import com.herawi.sigma.dto.MessageRequestDO;
+import com.herawi.sigma.exceptions.AccountNotFoundException;
+import com.herawi.sigma.models.Account;
 import com.herawi.sigma.models.Message;
 import com.herawi.sigma.repositories.MessageRepository;
+import com.herawi.sigma.utils.JWTTools;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
@@ -12,14 +18,32 @@ import java.util.stream.Collectors;
 public class MessageService {
 
     private final MessageRepository messageRepository;
+    private final AccountService accountService;
 
-    public MessageService(MessageRepository messageRepository) {
+    public MessageService(MessageRepository messageRepository, AccountService accountService) {
         this.messageRepository = messageRepository;
+        this.accountService = accountService;
     }
 
+    public Message addMessage(HttpServletRequest request, MessageRequestDO messageRequestDO){
+        String email = JWTTools.getUserEmailByJWT(request);
+        Account account = accountService.getAccountWithDetails(email);
+        if(account == null || !accountService.isAccountExistByUsername(messageRequestDO.getUsername())){
+            throw new AccountNotFoundException("account not found with provided username");
+        }
+        Message message = new Message();
+        message.setSenderUsername(account.getUserName());
+        message.setReceiverUsername(messageRequestDO.getUsername());
+        message.setText(messageRequestDO.getMessage());
+        return messageRepository.save(message);
+    }
 
-    public void addMessage(Message message){
-        messageRepository.save(message);
+    public Message addMessage(Message message){
+        if(!accountService.isAccountExistByUsername(message.getSenderUsername())  ||
+           !accountService.isAccountExistByUsername(message.getReceiverUsername())){
+            throw new AccountNotFoundException("account not found with provided username");
+        }
+        return messageRepository.save(message);
     }
 
     public Collection<Message> getAllMessageBySenderUsernameAndReceiverUsername(String senderUsername, String receiverUsername){
