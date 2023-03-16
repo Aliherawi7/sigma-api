e2dffa986fad3a65346b59582513b6fd0fde8915
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-16 16:05:06 +0430

    this dto is used to transfer the message information that is going to be saved in DB

--- src/main/java/com/herawi/sigma/dto/MessageRequestDO.java
+++ src/main/java/com/herawi/sigma/dto/MessageRequestDO.java
@@ -1,4 +1,31 @@
 package com.herawi.sigma.dto;
 
-public class MessageReqeustDTO {
+
+public class MessageRequestDO {
+    private String message;
+    private String username;
+
+    public MessageRequestDO() {
+    }
+
+    public MessageRequestDO(String message, String username) {
+        this.message = message;
+        this.username = username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
