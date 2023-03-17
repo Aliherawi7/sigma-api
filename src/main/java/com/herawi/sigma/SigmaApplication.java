5ba1be2dd4a53ecdccd4bd1c50a376c1f607870d
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-17 23:31:14 +0430

    some static friend requests added to each account for test purpose

--- src/main/java/com/herawi/sigma/SigmaApplication.java
+++ src/main/java/com/herawi/sigma/SigmaApplication.java
@@ -1,8 +1,11 @@
 package com.herawi.sigma;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.herawi.sigma.dto.AccountDTO;
 import com.herawi.sigma.dto.AccountRegistrationRequest;
+import com.herawi.sigma.models.Account;
 import com.herawi.sigma.models.Message;
+import com.herawi.sigma.repositories.AccountRepository;
 import com.herawi.sigma.services.AccountService;
 import com.herawi.sigma.services.MessageService;
 import org.springframework.boot.CommandLineRunner;
@@ -11,9 +14,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.persistence.TemporalType;
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.TemporalUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
 
@@ -46,17 +57,26 @@ public class SigmaApplication {
 						e.printStackTrace();
 					}
 				});
-				URL messagesJsonUrl = Thread.currentThread().getContextClassLoader().getResource("static\\messages.json");
+				URL messagesJsonUrl = Thread.currentThread()
+						.getContextClassLoader().getResource("static\\messages.json");
 				Message[] messagesArray = objectMapper.readValue(messagesJsonUrl, Message[].class);
+				AtomicInteger atomicInteger = new AtomicInteger(0);
 				Arrays.asList(messagesArray).forEach(item -> {
+					item.setDateTime(item.getDateTime().plusMinutes(atomicInteger.incrementAndGet()));
 					messageService.addMessage(item);
 				});
 
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+			List<AccountDTO> list = accountService.getAllAccount();
+			list.forEach(item -> {
 
-
+				for(int i = (int)(Math.random()*list.size()); i < list.size(); i++){
+					AccountDTO account = accountService.getAccount(list.get(i).getEmail());
+					accountService.addAsFriendById(item.getUserName(), account.getUserName());
+				}
+			});
 
 		};
 	}
