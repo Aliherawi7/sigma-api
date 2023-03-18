b2b680c13cba772f93f2f787a179759ec8ef4178
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-18 18:46:48 +0430

    query method return type updated to List

--- src/main/java/com/herawi/sigma/repositories/AccountRepository.java
+++ src/main/java/com/herawi/sigma/repositories/AccountRepository.java
@@ -2,12 +2,13 @@ package com.herawi.sigma.repositories;
 
 import com.herawi.sigma.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Collection;
+
+import java.util.List;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Account findByEmail(String email);
     Account findByUserName(String userName);
-    Collection<Account> findAllByNameOrLastName(String name, String lastName);
+    List<Account> findAllByNameOrLastName(String name, String lastName);
     boolean existsAccountByEmail(String email);
     boolean existsByEmailOrUserName(String email, String userName);
     boolean existsAccountByUserName(String username);
