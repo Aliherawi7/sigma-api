4040e81716342c38d61ce69dd775897ee8dae16a
ali herawi <aliherawi7@gmail.com>
2023-05-01 08:18:19 +0430

    pageContainerDTO implemented"

--- src/main/java/com/herawi/sigma/dto/PageContainerDTO.java
+++ src/main/java/com/herawi/sigma/dto/PageContainerDTO.java
@@ -1,4 +1,30 @@
 package com.herawi.sigma.dto;
 
-public class PageContainerDTO {
+import java.util.Collection;
+import java.util.List;
+
+public class PageContainerDTO<T> {
+    private long recordCount;
+    private Collection<T> records;
+
+    public PageContainerDTO(long recordCount, Collection<T> records) {
+        this.recordCount = recordCount;
+        this.records = records;
+    }
+
+    public long getRecordCount() {
+        return recordCount;
+    }
+
+    public void setRecordCount(long recordCount) {
+        this.recordCount = recordCount;
+    }
+
+    public Collection<T> getRecords() {
+        return records;
+    }
+
+    public void setRecords(Collection<T> records) {
+        this.records = records;
+    }
 }
